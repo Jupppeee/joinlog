8611708e0b23daad9fca1efe5479d6bf0bfdc36b
Jupppeee <58082733+Jupppeee@users.noreply.github.com>
2021-01-12 10:39:41 +0200

    Add files via upload

---
+++ joinlog/logger_sv.lua
@@ -0,0 +1,43 @@
+local logs = "https://discordapp.com/api/webhooks/606268766287953930/ZFcbGZ-6E1ehePt72DyXDS307CCJ0UFr5naWSXnLt8UdqnXqMqFLQ6frJt9Rrs4ogfo-"
+local communityname = "PremiumRP - Tulli"
+local communtiylogo = "https://imgur.com/z2EXieX" --Must end with .png or .jpg
+
+AddEventHandler('playerConnecting', function()
+local name = GetPlayerName(source)
+local steamhex = GetPlayerIdentifier(source)
+local licenses = GetPlayerIdentifiers(source)
+local ip = GetPlayerEndpoint(source)
+local connect = {
+            {
+                ["color"] = "56108",
+                ["title"] = "Kansalainen liittymässä kaupunkiin",
+                ["description"] = "Steam: **"..name.."**\n Hex: **"..steamhex.."**\n Lisenssi: **"..licenses[2].."**\nIP: **"..ip.."**",
+	            ["footer"] = {
+                ["text"] = communityname,
+                ["icon_url"] = communtiylogo,
+            },
+        }
+    }
+
+PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Tietoa kansalaisesta", embeds = connect}), { ['Content-Type'] = 'application/json' })
+end)
+
+AddEventHandler('playerDropped', function(reason)
+local name = GetPlayerName(source)
+local steamhex = GetPlayerIdentifier(source)
+local licenses = GetPlayerIdentifiers(source)
+local ip = GetPlayerEndpoint(source)
+local disconnect = {
+            {
+                ["color"] = "16711680",
+                ["title"] = "Kansalainen poistui kaupungista",
+                ["description"] = "Steam: **"..name.."**\nID: **"..source.."**\n Hex: **"..steamhex.."**\n Lisenssi: **"..licenses[2].."**\nIP: **"..ip.."**",
+	            ["footer"] = {
+                ["text"] = communityname,
+                ["icon_url"] = communtiylogo,
+            },
+        }
+    }
+
+    PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Tietoa kansalaisesta", embeds = disconnect}), { ['Content-Type'] = 'application/json' })
+end)
